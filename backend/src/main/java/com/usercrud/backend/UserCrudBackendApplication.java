2b692eb0dfd91b40f32e646c407946d3644f413d
jesu-m0 <jesusmd2000@gmail.com>
2025-07-24 10:46:10 +0200

    Initial commit: frontend and backend setup

---
+++ backend/src/main/java/com/usercrud/backend/UserCrudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.usercrud.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCrudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCrudBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/usercrud/backend/UserCrudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usercrud.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCrudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
